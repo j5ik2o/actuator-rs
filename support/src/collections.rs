8e5b1132b68b5211e4b529cc7e1bbb641644babf
Junichi Kato <j5ik2o@gmail.com>
2021-09-21 17:07:22 +0900

    wip

--- support/src/collections.rs
+++ support/src/collections.rs
@@ -1,11 +1,15 @@
 use anyhow::Result;
 use anyhow::anyhow;
 use std::collections::VecDeque;
+use std::sync::{Arc, Mutex, Condvar};
+use std::sync::mpsc::{Sender, Receiver, channel};
+use owning_ref::MutexGuardRef;
+use std::ops::Deref;
 
 pub trait Queue<E> {
   fn offer(&mut self, e: E) -> Result<bool>;
   fn poll(&mut self) -> Option<E>;
-  fn peek(&self) -> Option<&E>;
+  fn peek(&mut self) -> Option<&E>;
 }
 
 pub trait Deque<E>: Queue<E> {
@@ -15,13 +19,13 @@ pub trait Deque<E>: Queue<E> {
   fn poll_first(&mut self) -> Option<E>;
   fn poll_last(&mut self) -> Option<E>;
 
-  fn peek_first(&self) -> Option<&E>;
-  fn peek_last(&self) -> Option<&E>;
+  fn peek_first(&mut self) -> Option<&E>;
+  fn peek_last(&mut self) -> Option<&E>;
 }
 
 pub trait BlockingQueue<E>: Queue<E> {
-  fn put(&mut self, e: E);
-  fn take(&mut self) -> Result<E>;
+  fn put(&mut self, e: E) -> Result<()>;
+  fn take(&mut self) -> Option<E>;
 }
 
 #[derive(Debug, Clone)]
@@ -64,7 +68,7 @@ impl<E> Queue<E> for VecQueue<E> {
     self.values.pop_front()
   }
 
-  fn peek(&self) -> Option<&E> {
+  fn peek(&mut self) -> Option<&E> {
     self.values.front()
   }
 }
@@ -91,18 +95,71 @@ impl<E> Deque<E> for VecQueue<E> {
     self.values.pop_back()
   }
 
-  fn peek_first(&self) -> Option<&E> {
+  fn peek_first(&mut self) -> Option<&E> {
     self.peek()
   }
 
-  fn peek_last(&self) -> Option<&E> {
+  fn peek_last(&mut self) -> Option<&E> {
     self.values.back()
+  }
+}
+
+#[derive(Debug)]
+pub struct BlockingVecQueue<E> {
+  q: Mutex<VecQueue<E>>,
+  cv: Condvar,
+  peek: Option<E>,
+}
+
+impl<E> BlockingVecQueue<E> {
+  pub fn new() -> Self {
+    Self {
+      q: Mutex::new(VecQueue::new()),
+      cv: Condvar::new(),
+      peek: None
+    }
+  }
+}
+
+impl<E: Clone> Queue<E> for BlockingVecQueue<E> {
+  fn offer(&mut self, e: E) -> Result<bool> {
+    let mut lq = self.q.lock().unwrap();
+    lq.offer(e)
+  }
+
+  fn poll(&mut self) -> Option<E> {
+    let mut lq = self.q.lock().unwrap();
+    lq.poll()
+  }
+
+  fn peek(&mut self) -> Option<&E> {
+    let mut lq = self.q.lock().unwrap();
+    self.peek = lq.peek().map(|e| e.clone());
+    self.peek.as_ref()
+  }
+}
+
+impl<E: Clone> BlockingQueue<E> for BlockingVecQueue<E> {
+  fn put(&mut self, e: E) -> Result<()> {
+    let mut lq = self.q.lock().unwrap();
+    lq.offer(e);
+    self.cv.notify_one();
+    Ok(())
+  }
+
+  fn take(&mut self) -> Option<E> {
+    let mut lq = self.q.lock().unwrap();
+    while lq.values.len() == 0 {
+      lq = self.cv.wait(lq).unwrap();
+    }
+    lq.poll()
   }
 }
 
 #[cfg(test)]
 mod tests {
-  use crate::collections::{VecQueue, Queue, Deque};
+  use crate::collections::{VecQueue, Queue, Deque, BlockingVecQueue, BlockingQueue};
+  use std::sync::{Arc, Mutex};
 
   #[test]
   fn test_queue_1() {
@@ -145,5 +202,26 @@ mod tests {
     let result = vec_queue.offer(3);
     assert!(result.is_err());
 
+  }
+
+  #[test]
+  fn test_bq(){
+    let mut bq1 = Arc::new(Mutex::new(BlockingVecQueue::new()));
+    let mut bq2 = bq1.clone();
+
+    use std::thread;
+
+    let handler = thread::spawn(move || {
+      println!("start: lock");
+      let mut bq2_g = bq2.lock().unwrap();
+      println!("finish: lock");
+      let n = bq2_g.take();
+      println!("n = {:?}", n);
+    });
+    {
+      let mut bq1_g = bq1.lock().unwrap();
+      bq1_g.put(1);
+    }
+    handler.join().unwrap();
   }
 }
