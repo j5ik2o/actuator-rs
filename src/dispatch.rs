fa484d963e09064bc5c5f5d79b3c3065743ce2a7
Junichi Kato <j5ik2o@gmail.com>
2022-09-19 01:30:08 +0900

    wip

--- src/dispatch.rs
+++ src/dispatch.rs
@@ -1,5 +1,6 @@
 pub mod any_message;
 mod envelope;
 mod mail_box;
+mod mailbox_type;
 mod message;
 mod message_queue;
---
+++ src/dispatch/mailbox_type.rs
@@ -0,0 +1,38 @@
+use crate::actor::actor_ref::ActorRef;
+use crate::dispatch::message_queue::MessageQueue;
+use crate::queue::QueueType;
+use std::time::Duration;
+
+pub enum MailboxType {
+  Unbounded,
+  Bounded { capacity: usize, push_time_out: Duration },
+}
+
+impl MailboxType {
+  pub fn of_unbounded() -> Self {
+    MailboxType::Unbounded
+  }
+
+  pub fn of_bounded(capacity: usize, push_time_out: Duration) -> Self {
+    MailboxType::Bounded {
+      capacity,
+      push_time_out,
+    }
+  }
+}
+
+pub trait MailboxTypeBehavior {
+  fn create(&self, owner: Option<ActorRef> /* system: Option<ActorSystem> */) -> MessageQueue;
+}
+
+impl MailboxTypeBehavior for MailboxType {
+  fn create(&self, owner: Option<ActorRef>) -> MessageQueue {
+    match self {
+      MailboxType::Unbounded => MessageQueue::of_unbounded_with_queue_type(QueueType::MPSC),
+      MailboxType::Bounded { capacity, .. } => {
+        MessageQueue::of_bounded_with_queue_type_with_num_elements(QueueType::MPSC, *capacity)
+      }
+      _ => panic!(""),
+    }
+  }
+}
