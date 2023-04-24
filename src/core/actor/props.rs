424f874369c0b8e3678674ad5e6b829f6d6db32c
Junichi Kato <j5ik2o@gmail.com>
2023-04-24 10:51:32 +0900

    wip

--- src/core/actor/props.rs
+++ src/core/actor/props.rs
@@ -8,7 +8,6 @@ use std::rc::Rc;
 
 pub trait Props<Msg: Message>: Debug {
   fn new_actor(&self) -> Rc<RefCell<dyn ActorMutableBehavior<Msg>>>;
-  // fn to_any(&self) -> Rc<dyn Props<AnyMessage>>;
 }
 
 #[derive(Debug, Clone)]
@@ -26,10 +25,6 @@ impl<Msg: Message> Props<Msg> for MockProps<Msg> {
   fn new_actor(&self) -> Rc<RefCell<dyn ActorMutableBehavior<Msg>>> {
     Rc::new(RefCell::new(MockActorMutable { p: PhantomData }))
   }
-
-  // fn to_any(&self) -> Rc<dyn Props<AnyMessage>> {
-  //   Rc::new(AnyProps::new(Rc::new(MockProps::<AnyMessage>::new())))
-  // }
 }
 
 #[derive(Debug, Clone)]
@@ -47,10 +42,6 @@ impl<Msg: Message> Props<AnyMessage> for AnyProps<Msg> {
   fn new_actor(&self) -> Rc<RefCell<dyn ActorMutableBehavior<AnyMessage>>> {
     Rc::new(RefCell::new(AnyMessageActorWrapper::new(self.underlying.new_actor())))
   }
-
-  // fn to_any(&self) -> Rc<dyn Props<AnyMessage>> {
-  //   Rc::new(self.clone())
-  // }
 }
 
 #[derive(Debug, Clone)]
@@ -68,13 +59,6 @@ impl<Msg: Message> Props<Msg> for SingletonProps<Msg> {
   fn new_actor(&self) -> Rc<RefCell<dyn ActorMutableBehavior<Msg>>> {
     self.actor.clone()
   }
-
-  // fn to_any(&self) -> Rc<dyn Props<AnyMessage>> {
-  //   Rc::new(AnyProps::new(Rc::new(self.clone())))
-  //   // Rc::new(SingletonProps::new(Rc::new(RefCell::new(AnyMessageActorWrapper::new(
-  //   //   self.actor.clone(),
-  //   // )))))
-  // }
 }
 
 pub struct FunctionProps<Msg: Message> {
@@ -109,8 +93,4 @@ impl<Msg: Message> Props<Msg> for FunctionProps<Msg> {
   fn new_actor(&self) -> Rc<RefCell<dyn ActorMutableBehavior<Msg>>> {
     (*self.actor_f.clone())()
   }
-
-  // fn to_any(&self) -> Rc<dyn Props<AnyMessage>> {
-  //   Rc::new(AnyProps::new(Rc::new(self.clone())))
-  // }
 }
